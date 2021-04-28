9abffc63d51c5897b62974b6a5e3f3ce754c7fa4
lola caillaud <61966450+lolacld@users.noreply.github.com>
2021-04-28 11:42:22 +0200

    add connexion database file, security functions

---
+++ helper/DatabaseHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+class DatabaseHelper
+{
+
+    private static $db_host = 'localhost';
+    private static $db_name = 'mytotool';
+    private static $db_user = 'root';
+    private static $db_password = '';
+
+    private static $DB;
+
+    public static function getDB()
+    {
+        if (is_null(self::$DB)) {
+            self::$DB = self::DBConnect();
+        }
+
+        return self::$DB;
+    }
+
+    private static function DBConnect()
+    {
+        return new PDO('mysql:host=' . self::$db_host . ';dbname=' . self::$db_name, self::$db_user, self::$db_password);
+    }
+}
